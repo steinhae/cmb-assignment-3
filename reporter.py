3fa659e22f55631902f62ab603e1555fab7d2d81
Hannes <fbsteinhae@gmx.de>
2017-02-23 17:45:43 +0100

    Added command line arguments

--- reporter.py
+++ reporter.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import csv
+import argparse
 from geopy.distance import vincenty
+import textwrap
 
 class CircleGeofence():
     def __init__(self, name, latitude, longitude, radius):
@@ -11,29 +13,39 @@ class CircleGeofence():
         self.name = name
 
 def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-r', '--report',
+                        help='available report types\n'
+                            '1 - statistics\n'
+                            '2 - write measurements for geofences\n',
+                        required=True)
+    args = parser.parse_args()
+
     measurements, data_fields = read_csv('measurements_clean.csv')
-    print_number_of_measurements(measurements)
-    write_geofence_measurements(measurements, data_fields)
+
+    rn = (vars(args))['report']
+    if rn == '1':
+        print_measurement_statistics_report(measurements)
+    elif rn == '2':
+        write_geofence_measurements(measurements, data_fields)
+
 
 def write_geofence_measurements(measurements, data_fields):
-    print_seperator()
     geofences = []
     geofences.append(CircleGeofence('munich_area', 48.138561, 11.573757, 7000))
     geofences.append(CircleGeofence('tum_mi', 48.262547, 11.667838, 150))
     for geofence in geofences:
         write_csv_for_geofence(measurements, geofence, data_fields)
 
-def print_number_of_measurements(measurements):
+def print_measurement_statistics_report(measurements):
     overall = len(measurements)
     wifi = 0
     for line in measurements:
         if line['radiotech'] == '0':
             wifi = wifi + 1
-    print_seperator()
     print '# measurements =', overall
-    print '# wifi = ', wifi, '(' + "%.2f" % (float(wifi)/float(overall)) + '%)'
-    print '# cellular =', overall-wifi, '(' + "%.2f" % (float(overall-wifi)/float(overall)) + '%)'
-
+    print '# wifi = ', wifi, '(' + "%.2f" % (float(wifi)/float(overall)*100) + '%)'
+    print '# cellular =', overall-wifi, '(' + "%.2f" % (float(overall-wifi)/float(overall)*100  ) + '%)'
 
 def write_csv_for_geofence(measurements, geofence, data_fields):
     result = get_measurements_per_geofence(geofence, measurements)
@@ -90,9 +102,5 @@ def write_csv(file_name, measurements, data_fields):
         writer.writeheader()
         writer.writerows(measurements)
 
-def print_seperator():
-    print '---------------------------------------'
-
 if __name__ == '__main__':
     main()
-    print_seperator()
