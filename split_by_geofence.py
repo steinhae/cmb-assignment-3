6788e9219e394eaa5ed3c55941045a956df09e48
Patrick Sattler <sattler.patrick@posteo.de>
2017-03-06 14:49:43 +0100

    make split_by_geofence a autonomous script so that it can be called without the reporter script

--- split_by_geofence.py
+++ split_by_geofence.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
+from __future__ import print_function
 
 import os
 
-from .util import write_csv
-from .constants import geofences, generated_measurements_path
+from .util import write_csv, read_csv
+from .constants import geofences, generated_measurements_path, measurements_path
 from .geofence_util import get_measurements_per_geofence
 
 
@@ -18,3 +20,13 @@ def write_csv_for_geofence(measurements, geofence, data_fields):
                              geofence.name + '_radius_' + str(geofence.radius) + 'm.csv')
     write_csv(file_name, result, data_fields)
     print('{} measurements have been written to {}'.format(len(result), file_name))
+
+
+def main():
+    measurements, data_fields = read_csv(os.path.join(measurements_path, 'measurements_clean.csv'))
+    write_geofence_measurements(measurements, data_fields)
+    print('done')
+
+
+if __name__ == '__main__':
+    main()
